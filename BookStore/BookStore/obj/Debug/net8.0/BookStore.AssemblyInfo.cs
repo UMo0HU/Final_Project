8535123fdc1ee0ef30d31bd057baa845092ec92b
Mohamed_Abdo <bata6645@gmail.com>
2025-04-22 14:56:19 +0200

    MergeWishlist

--- BookStore/BookStore/obj/Debug/net8.0/BookStore.AssemblyInfo.cs
+++ BookStore/BookStore/obj/Debug/net8.0/BookStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a310ad3ca450eb12a9d8ea5e39f666612336c26a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+853b01e67dc6ce4deba304b0a170d0d27cef6484")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
