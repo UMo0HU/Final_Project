5bc99e0b06f898d1db08a1ebe584fbde36d97908
NADA <n.hassan2334@nu.edu.eg>
2025-05-01 19:28:19 -0700

    added frontend& backend for Book Management page

---
+++ BookStore/BookStore/wwwroot/js/ManageBooks.js
@@ -0,0 +1,12 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const deleteButtons = document.querySelectorAll(".btn-delete");
+
+    deleteButtons.forEach(button => {
+        button.addEventListener("click", function () {
+            const bookTitle = this.getAttribute("data-title");
+            if (confirm(`Are you sure you want to delete "${bookTitle}"?`)) {
+                alert("Book deleted (simulation)");
+            }
+        });
+    });
+});
