31f47b357df774eb90acbdc4d6831c1b8b7973ad
Mohamed_Abdo <bata6645@gmail.com>
2025-05-06 14:36:56 +0300

    feature two added (order track and order list)

--- BookStore/BookStore/obj/Debug/net8.0/BookStore.AssemblyInfo.cs
+++ BookStore/BookStore/obj/Debug/net8.0/BookStore.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e06be703e94b2a66d1234df171d91e43573d6b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08d8d7f8c9ba4307006f02525f989815b255f60e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
