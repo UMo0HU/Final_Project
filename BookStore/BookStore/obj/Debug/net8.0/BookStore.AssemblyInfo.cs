b53da1c63bbdea375170c1b2d7884a795067bf73
Mohamed_Abdo <bata6645@gmail.com>
2025-05-06 08:18:55 +0300

    first feature (dashboard successfully added)

--- BookStore/BookStore/obj/Debug/net8.0/BookStore.AssemblyInfo.cs
+++ BookStore/BookStore/obj/Debug/net8.0/BookStore.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf32a985c63ef500bfcff4bc10763509747bb08b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e06be703e94b2a66d1234df171d91e43573d6b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
