daeae9e6862272c1d4a5711499d0c2511a696cea
Mohamed_Abdo <bata6645@gmail.com>
2025-05-22 17:55:35 +0300

    small update on db

---
+++ BookStore/BookStore/Migrations/20250522143411_deletedCartItemQuantity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class deletedCartItemQuantity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "CartItems");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                table: "CartItems",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
